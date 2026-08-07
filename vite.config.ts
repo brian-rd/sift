a69d355f7e792c48a150b10604631cdfc18a3e4b
brian-rd <theblockycharacter@gmail.com>
2026-08-07 01:37:37 +0100

    scaffold Svelte and Tauri app

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+export default defineConfig({
+  plugins: [svelte()],
+  clearScreen: false,
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: '127.0.0.1'
+  }
+});
